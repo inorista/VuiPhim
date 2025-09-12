355dafa58f96caeaf1fa8f253feb5f278cec0a26
Ngo Tu <ngoquoctu@teams.airlab.dev>
2025-09-12 09:40:23 +0700

    upgrade compileSdk android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
 android {
     namespace = "com.dev.vuiphim"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -40,7 +40,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
